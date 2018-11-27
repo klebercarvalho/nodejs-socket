5b72d3e51aeb301715c0daaeed4ce975708c0408
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-11-27 15:15:48 -0200

    Update

--- src/client.js
+++ src/client.js
@@ -4,12 +4,12 @@ const HOST = '127.0.0.1';
 const PORT = 6969;
 const args = process.argv.slice(2);
 
+
 const client = new net.Socket();
 client.connect(PORT, HOST, () => {
-  console.log(`CONNECTED TO: ${HOST}:${PORT}`);
+  console.log(`CONNECTED TO: ${  HOST  }:${  PORT}`);
   // Write a message to the socket as soon as the client is connected,
   // the server will receive it as message from the client
-
   if (typeof args[0] !== 'undefined' && args[0] !== null) {
     client.write(args[0]);
   } else {
@@ -20,7 +20,7 @@ client.connect(PORT, HOST, () => {
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
 client.on('data', (data) => {
-  console.log(`DATA: ${data}`);
+  console.log(`DATA: ${  data}`);
   // Close the client socket completely
   client.destroy();
 });
